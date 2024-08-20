5f80376bf3c64cc42d342da6bcc5449f9b37e93c
Nick Wallace <nwallace@fyberstudios.com>
2024-08-20 12:29:44 -0500

    adding length check

--- src/buffer.go
+++ src/buffer.go
@@ -956,8 +956,7 @@ func thirdPartyBuffer(streamID int, playlistID string, useBackup bool, backupNum
 			if !useBackup || (useBackup && backupNumber >= 0 && backupNumber <= 3) {
 				backupNumber = backupNumber + 1
 				if playlist != nil {
-					if playlist.Streams != nil {
-						fmt.Println("PLAYLIST: ", playlist.Streams)
+					if playlist.Streams != nil && len(playlist.Streams) > 0 {
 						if playlist.Streams[streamID].BackupChannel1URL != "" || playlist.Streams[streamID].BackupChannel2URL != "" || playlist.Streams[streamID].BackupChannel3URL != "" {
 							thirdPartyBuffer(streamID, playlistID, true, backupNumber)
 						}
