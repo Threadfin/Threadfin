62ea30f0aca2138594c647d9f276231fe74cbb7b
Nick Wallace <nwallace@fyberstudios.com>
2024-08-20 12:44:35 -0500

    ughhhh

--- src/buffer.go
+++ src/buffer.go
@@ -957,6 +957,7 @@ func thirdPartyBuffer(streamID int, playlistID string, useBackup bool, backupNum
 				backupNumber = backupNumber + 1
 				if playlist != nil {
 					if playlist.Streams != nil && len(playlist.Streams) > 0 {
+						fmt.Println("STREAMS:", playlist.Streams)
 						if playlist.Streams[streamID].BackupChannel1URL != "" || playlist.Streams[streamID].BackupChannel2URL != "" || playlist.Streams[streamID].BackupChannel3URL != "" {
 							thirdPartyBuffer(streamID, playlistID, true, backupNumber)
 						}
